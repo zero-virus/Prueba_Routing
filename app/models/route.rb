e1670c6b1b06e4a704c30ba537b6fc1e6d464332
Camila Solis <camilasolis@MacBook-Air.local>
2019-11-28 18:37:55 -0300

    Added more validator, for Route
    Added validators for the quantity of stops and if the cities are within the driver's

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,6 +4,8 @@ class Route < ApplicationRecord
 
   validates :load_type, inclusion: {in: %w(refrigerada general)}
   validate :validate_load_type, :validate_capacity, on: :create
+  validate :validate_stops, :validate_cities, on: :create# , :validate_times_overlap
+
   def validate_load_type
     vehicle = Vehicle.find(self.vehicle_id.to_s)
     if vehicle.load_type != self.load_type
@@ -15,5 +17,19 @@ class Route < ApplicationRecord
     vehicle = Vehicle.find_by(self.vehicle_id.to_s)
     if vehicle.capacity < self.load_sum
         errors.add(:capacity, "Exceeds maximum capacity")
+    end
+  end
+
+  def validate_stops
+    driver = Driver.find_by(self.driver_id.to_s)
+    if driver.max_stops < self.stop_amount
+        errors.add(:stop_amount, "To many stops for this driver")
+    end
+  end
+
+  def validate_cities
+    driver = Driver.find(self.driver_id)
+    unless (self.cities.lines.to_a - driver.cities.lines.to_a).empty?
+        errors.add(:cities, "The driver don't go to that city")
     end
   end
