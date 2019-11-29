22c30994c67df1ff94286917eeb6e2bdefeeb85a
Camila Solis <camilasolis@MacBook-Air.local>
2019-11-29 02:22:42 -0300

    Fixed the comments

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,6 +1,4 @@
-'''
-Relationships and validations for de Route model
-'''
+#Relationships and validations for de Route model
 class Route < ApplicationRecord
   belongs_to :vehicle
   belongs_to :driver
@@ -9,45 +7,41 @@ class Route < ApplicationRecord
   validate :validate_load_type, :validate_capacity, on: :create
   validate :validate_stops, :validate_cities, :validate_times_overlap
 
-  '''
-  Validate that the vehicule have the correct load_type
-  '''
+#  Validate that the vehicule have the correct load_type
   def validate_load_type
     vehicle = Vehicle.find(self.vehicle_id.to_s)
     if vehicle.load_type != self.load_type
       errors.add(:load_type, "This vehicle can't load this type of object ")
     end
   end
-'''
-Validate that the vehicle have the correct capacity
-'''
+
+# Validate that the vehicle have the correct capacity
   def validate_capacity
     vehicle = Vehicle.find_by(self.vehicle_id.to_s)
     if vehicle.capacity < self.load_sum
         errors.add(:capacity, "Exceeds maximum capacity")
     end
   end
-'''
-Validated that the quantity of stops is acceptable for the driver
-'''
+
+#Validated that the quantity of stops is acceptable for the driver
   def validate_stops
     driver = Driver.find_by(self.driver_id.to_s)
     if driver.max_stops < self.stop_amount
         errors.add(:stop_amount, "To many stops for this driver")
     end
   end
-'''
-Validate that the cities are in the zone of delivery of the driver
-'''
+
+#Validate that the cities are in the zone of delivery of the driver
+
   def validate_cities
     driver = Driver.find(self.driver_id)
     unless (self.cities.lines.to_a - driver.cities.lines.to_a).empty?
         errors.add(:cities, "The driver don't go to that city")
     end
   end
-'''
-Validate that the driver and the vehicle have free time in the hours of the delivery
-'''
+
+  #Validate that the driver and the vehicle have free time in the hours of the delivery
+
   def validate_times_overlap
     if Routes.exist(driver_id: self.driver_id.to_i) || Routes.exist(vehicle_id: self.vehicle_id.to_i)
         routes_all_driver = Routes.where(driver_id: self.driver_id.to_i)
@@ -66,7 +60,6 @@ Validate that the driver and the vehicle have free time in the hours of the deli
         if save_routes != []
             errors.add(:start_at,:ends_at, "The driver or vehicule are not available in this hour")
         end
-
     end
   end
 end
